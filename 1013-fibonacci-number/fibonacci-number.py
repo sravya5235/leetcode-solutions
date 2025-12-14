0165c5d4781d107f3ba0f2e19e4314760195e55d
sravya5235 <143372401+sravya5235@users.noreply.github.com>
2025-12-14 20:40:27 +0530

    Time: 39 ms (62.34%) | Memory: 17.5 MB (88.85%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,10 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        
-        a, b = 0, 1
-        for _ in range(2, n + 1):
-            a, b = b, a + b
-        
-        return b
+        def fast_doubling(n):
+            if n == 0:
+                return (0, 1)
+            a, b = fast_doubling(n // 2)
+            c = a * (2 * b - a)
+            d = a * a + b * b
+            if n % 2 == 0:
+                return (c, d)
+            else:
+                return (d, c + d)
+
+        return fast_doubling(n)[0]
