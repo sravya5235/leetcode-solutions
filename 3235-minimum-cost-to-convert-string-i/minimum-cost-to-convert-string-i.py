5f1a8b290b0bd65095484031a6e6512a168b8033
sravya5235 <143372401+sravya5235@users.noreply.github.com>
2026-01-29 20:38:04 +0530

    Time: 847 ms (54.43%) | Memory: 21.1 MB (28.27%) - LeetSync

---
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minimumCost(self, source: str, target: str,
+                    original: list[str], changed: list[str], cost: list[int]) -> int:
+        
+        INF = 10**18
+        dist = [[INF] * 26 for _ in range(26)]
+        
+        for i in range(26):
+            dist[i][i] = 0
+        
+        # direct conversions
+        for o, c, w in zip(original, changed, cost):
+            u = ord(o) - ord('a')
+            v = ord(c) - ord('a')
+            dist[u][v] = min(dist[u][v], w)
+        
+        # Floyd–Warshall
+        for k in range(26):
+            for i in range(26):
+                for j in range(26):
+                    if dist[i][k] + dist[k][j] < dist[i][j]:
+                        dist[i][j] = dist[i][k] + dist[k][j]
+        
+        # compute total cost
+        ans = 0
+        for s, t in zip(source, target):
+            if s == t:
+                continue
+            u = ord(s) - ord('a')
+            v = ord(t) - ord('a')
+            if dist[u][v] == INF:
+                return -1
+            ans += dist[u][v]
+        
+        return ans
