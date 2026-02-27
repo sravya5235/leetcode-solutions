4cbb30f11924cb1cefa7cbcdc619219f445caedc
sravya5235 <143372401+sravya5235@users.noreply.github.com>
2026-02-27 18:00:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (77.10%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -1,23 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
-class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        
-        def isMirror(left, right):
-            if not left and not right:
-                return True
-            if not left or not right:
-                return False
-            
-            return (
-                left.val == right.val and
-                isMirror(left.left, right.right) and
-                isMirror(left.right, right.left)
-            )
-        
-        return isMirror(root.left, root.right)
+class Solution(object):
+    def isSymmetric(self, root):
+        # Special case...
+        if not root:
+            return true;
+        # Return the function recursively...
+        return self.isSame(root.left, root.right)
+    # A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
+    def isSame(self, leftroot, rightroot):
+        # If both root nodes are null pointers, return true...
+        if leftroot == None and rightroot == None:
+            return True
+        # If exactly one of them is a null node, return false...
+        if leftroot == None or rightroot == None:
+            return False
+        # If root nodes haven't same value, return false...
+        if leftroot.val != rightroot.val:
+            return False
+        # Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
+        return self.isSame(leftroot.left, rightroot.right) and self.isSame(leftroot.right, rightroot.left)
