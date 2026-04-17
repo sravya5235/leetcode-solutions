30d1d01147c1bed03553883a8f70e46b4461a2c3
CHALLAGOLLA SRAVYA CHOWDARY <sravya5235@gmail.com>
2026-04-17 16:50:17 +0530

    Time: 203 ms (90.70%) | Memory: 41.9 MB (73.64%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
@@ -1,16 +1,9 @@
 class Solution:
     def minMirrorPairDistance(self, nums: List[int]) -> int:
-        def reversion(x):
-            res = 0
-            while x > 0:
-                x, d = divmod(x, 10)
-                res = 10 * res + d
-            return res
-        hash_map = {}
-        mod = inf 
-        for i, x in enumerate(nums):
-            rev = reversion(x)
-            if x in hash_map:
-                mod = min(mod, i - hash_map[x])
-            hash_map[rev] = i
-        return -1 if mod == inf else mod
+        seen = {}
+        mn_diff = float('inf')
+        for i, val in enumerate(nums):
+            if val in seen:
+                mn_diff = min(mn_diff, i - seen[val])
+            seen[int(str(val)[::-1])] = i
+        return mn_diff if mn_diff != float('inf') else -1
